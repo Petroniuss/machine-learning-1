f8ed6fcd3b36c3b53689a7c8f3af4106aecb65ef
Petroniuss <patryk.wojtyczek@ibm.com>
2021-04-17 18:06:04 +0200

    added kazar dataset

---
+++ lab3/main.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import glob
+import os
+import numpy as np
+
+kazar_dataset_dir = 'datasets/kazar'
+img_normalized_size = (120, 120)
+# todo we could also crop images from kazar dataset
+
+def convert_img_to_vector(img):
+    return np.asarray(img).reshape((-1, 1))
+
+
+def convert_img_vector_to_img(img_vector):
+    M = img_vector.reshape(img_normalized_size)
+
+    return Image.fromarray(M)
+
+
+def load_dataset(dataset_dir):
+    """
+        assumes '${class_name}_.*'
+    """
+    dataset = []
+    for path in glob.glob(f'{dataset_dir}/*'):
+        img = Image.open(path)
+        file_name = os.path.basename(path)
+        class_name = file_name.partition('_')[0]
+
+        dataset.append((class_name, img))
+    return dataset
+
+
+def normalize_dataset(dataset):
+    transformed = []
+    for class_name, img in dataset:
+        resized_img = img.resize(img_normalized_size)
+        normalized_img = resized_img.convert('L')
+
+        transformed.append((class_name, normalized_img))
+
+    return transformed
+
+
+if __name__ == '__main__':
+    ds = load_dataset(kazar_dataset_dir)
+    ds = normalize_dataset(ds)
